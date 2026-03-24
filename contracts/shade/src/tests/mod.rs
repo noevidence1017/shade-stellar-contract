af0d0546f073dccf99383fb7f9c130b1d11df5e9
Lawal Abubakar Babatunde <utility4all@gmail.com>
2026-03-24 09:56:11 +0100

    feat: implement Draft Invoices and comprehensive tests #139

--- contracts/shade/src/tests/mod.rs
+++ contracts/shade/src/tests/mod.rs
@@ -6,6 +6,7 @@ pub mod test_admin_payment;
 pub mod test_admin_transfer;
 pub mod test_fees;
 pub mod test_invoice;
+pub mod test_draft_invoice;
 pub mod test_invoice_partial_refund;
 pub mod test_invoice_signed;
 pub mod test_invoice_void;
